088a54311317b88f94b17c1e00164bc5326b9fa8
Dolf Schimmel (Freeaqingme) <dolfschimmel@gmail.com>
2012-11-11 02:58:49 +0100

    Initial code base

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ EnriseRestfulApi
 
 This Module aims to provide an easy and HMVC-like controller structure
 to easily construct a Restful Api with.
+
+For an example of how this module is used, please see the EnriseSkeletonRestfulApi project.
---
+++ src/Enrise/RestfulApi/Mvc/Router/Http/RestRouteListener.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Enrise RestfulApi  (http://enrise.com/)
+ *
+ * @link      https://github.com/Enrise/EnriseRestfulApi for the canonical source repository
+ * @copyright Copyright (c) 2012 Dolf Schimmel - Freeaqingme (dolfschimmel@gmail.com)
+ * @copyright Copyright (c) 2012 Enrise (www.enrise.com)
+ * @license   New BSD License, see LICENSE.MD
+ */
+
+namespace Enrise\RestfulApi\Mvc\Router\Http;
+
+use Enrise\RestfulApi\Mvc\Router\Http\Rest as RestRoute;
+use Zend\Mvc\MvcEvent;
+
+class RestRouteListener
+{
+
+    public function setupRoutes(MvcEvent $e)
+    {
+        $options = $e->getApplication()->getServiceManager()->get('Config');
+        if (!isset($options['router']['apiRoutes'])) {
+            return;
+        }
+
+        $sm = $e->getApplication()->getServiceManager();
+        $router = $e->getApplication()->getServiceManager()->get('router');
+
+        foreach($options['router']['apiRoutes'] as $key => $routeOptions) {
+            $route = RestRoute::factory($routeOptions, $sm);
+            $router->addRoute($key, $route);
+        }
+    }
+}
